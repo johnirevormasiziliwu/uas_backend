0788db9a460c336af3d556964b81a92ca27b4dd4
johan <johanwirasugianto@gmail.com>
2022-11-14 09:55:59 +0700

    Tugas 2

---
+++ src/main/java/ukrim/co/id/serversideukrim/model/Country.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ukrim.co.id.serversideukrim.model;
+
+/**
+ *
+ * @author MSI-JO
+ */
+public class Country {
+    private Long id; // PK
+    private String code; // ID (Not Null)
+    private String name; // Indonesia (Not NULL)
+}
