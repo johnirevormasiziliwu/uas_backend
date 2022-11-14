4f57e029702337cde78e7854ff99cb23c8448b23
johan <johanwirasugianto@gmail.com>
2022-11-14 09:48:09 +0700

    CRUD Region

--- src/main/java/ukrim/co/id/serversideukrim/model/Region.java
+++ src/main/java/ukrim/co/id/serversideukrim/model/Region.java
@@ -5,13 +5,50 @@
  */
 package ukrim.co.id.serversideukrim.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author MSI-JO
  */
+@Entity
+@Table(name = "tb_region")
 public class Region {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // PK
+    
+    @Column(nullable = false, name = "region_name")
     private String name; // Not_null
+
+    public Region() {
+    }
+
+    public Region(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
